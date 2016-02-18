d82e624b47d198ddd93c2ff7f624146d508291f4
SharathChitmalla <sharathchandra@siu.edu>
2016-02-18 00:12:31 -0600

    Homepage
    Search Module

---
+++ AIRLINE_BOOKING_MGMT_SYSTEM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using atm.USER_FORMS;
+
+namespace atm
+{
+    public class AIRLINE_BOOKING_MGMT_SYSTEM: Form
+    {
+        string uname = "";
+        public AIRLINE_BOOKING_MGMT_SYSTEM()
+        {
+            InitializeComponent();
+        }
+        public AIRLINE_BOOKING_MGMT_SYSTEM(string s)
+        {
+            uname = s;
+            InitializeComponent();
+        }
+
+      
+
+        private void SearchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DisposeAllButThis();
+            Search sc = new Search(lbshwuser.Text);
+            sc.MdiParent = this;
+            sc.Show();
+        }
+    }
+}
