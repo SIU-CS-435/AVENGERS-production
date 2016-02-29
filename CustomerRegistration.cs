8b4989c9a8d184206f2b3e79b9e3ddc59a97140b
RajaYerram <raja.yerram@siu.edu>
2016-02-29 14:22:52 -0600

    Customer Registration Page
    Modifications to the registration page has been done!

--- CustomerRegistration.cs
+++ CustomerRegistration.cs
@@ -48,3 +48,92 @@ namespace atm
                 this.BeginInvoke(new MethodInvoker(Close));
             }
         }
+        private void btnsubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int valid1 = 0;
+                if (cbgen.Text == "Select Gender" || cbssn.Text == "Select SSN type")
+                {
+                    valid = 1;
+                }
+                foreach (Control tb in this.Controls)
+                {
+                    if (tb is TextBox)
+                    {
+                        valid1 = bussiness_accesslayer.null_validation(tb.Text);
+                        if (valid1==1 ||valid == 1)
+                        {
+                            MessageBox.Show("Empty Fields Not Allowed", "Error");
+                            break;
+                        }
+                        if (valid == 2)
+                        {
+                            MessageBox.Show("Validations Not Satisfied", "Error");
+                            break;
+                        }
+                    }
+                }
+                if (valid == 0 && valid1 == 0)
+                {
+                    string encrpyt = "10101010"+tbpass.Text;
+
+                    string q = "INSERT INTO CUSTOMER_INFO (CNAME,DOB,ADDRESS,PHONE,EMAIL,PASSWORD,SSN_TYPE,SSN_NO,GENDER) VALUES ('" + tbname.Text + "','" + dob.Value.Date.ToShortDateString() + "','" + tbadd.Text + "','" + tbphone.Text + "','" + tbemail.Text + "','" + encrpyt + "','" + cbssn.Text + "','" + tbssn.Text + "','" + cbgen.Text + "')";
+                    dataaccess_layer.ProcessQuery(q);
+                    MessageBox.Show("Registration is Successful", "Congrats");
+                    MessageBox.Show("Your Email will be user ID", "Remember");
+                    btnclear_Click(btnclear, null);
+                    this.Close();
+                    USER_LOGIN ul = new USER_LOGIN();
+                    ul.Show();
+                }
+                valid = 0;
+            }
+
+            catch (Exception ex)
+            {
+                bussiness_accesslayer.LogErrorFile(ex.Message + DateTime.Now.ToLongDateString() + DateTime.Now.ToLongTimeString());
+                MessageBox.Show("Internal Error Occured", "Sorry");
+                this.BeginInvoke(new MethodInvoker(Close));
+            }
+        }
+        private void Customer_Registration_Load(object sender, EventArgs e)
+        {  
+            
+            dob.MaxDate = DateTime.Now.Date;
+            cbgen.Text = "Select Gender";
+            cbgen.Items.Add("Male");
+            cbgen.Items.Add("Female");
+            cbssn.Text = "Select SSN type";
+            cbssn.Items.Add("Driving License");
+            cbssn.Items.Add("Voter Id");
+            cbssn.Items.Add("Pancard");
+            cbssn.Items.Add("Aadhar Card");
+        }
+
+        private void tbemail_Leave(object sender, EventArgs e)
+        {
+            eemail.Clear();
+            int email_validate = bussiness_accesslayer.tbemail_Validating(tbemail.Text);
+            if (email_validate == 1)
+            {
+                eemail.SetError(tbemail, "Enter Valid Email");
+                valid = 2;
+            }
+            if (email_validate == 2)
+            {
+                eemail.SetError(tbemail, "Email already Registered");
+                valid = 2;
+            }
+        }
+
+        private void tbname_Leave(object sender, EventArgs e)
+        {
+            ename.Clear();
+            int name_validate = bussiness_accesslayer.name_validation(tbname.Text);
+            if (name_validate == 1)
+            {
+                ename.SetError(tbname, "Name cannot be alphanumberic && Special Characters Not allowed");
+                valid = 2;
+            }
+        }
