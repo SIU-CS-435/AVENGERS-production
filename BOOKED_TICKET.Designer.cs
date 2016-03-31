cf832bea81c22d0071875f8650b391db3ed7c48a
snehathota <snehathota@siu.edu>
2016-03-30 23:18:26 -0500

    Booked Ticket Desginer Page
    The CSS [age for booked ticket

--- BOOKED_TICKET.Designer.cs
+++ BOOKED_TICKET.Designer.cs
@@ -47,3 +47,54 @@ namespace atm.USER_FORMS
             this.dgviewbooked.Size = new System.Drawing.Size(992, 281);
             this.dgviewbooked.TabIndex = 0;
             this.dgviewbooked.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgviewbooked_CellContentClick);
+       	    // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1023, 58);
+            this.pictureBox1.TabIndex = 72;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(39, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 24);
+            this.label1.TabIndex = 73;
+            this.label1.Text = "Booked Ticket History";
+            // 
+            // BOOKED_TICKET
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1016, 426);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgviewbooked);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BOOKED_TICKET";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BOOKED_TICKET";
+            this.Load += new System.EventHandler(this.BOOKED_TICKET_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgviewbooked)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgviewbooked;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
