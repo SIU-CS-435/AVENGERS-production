0ee8482f4483b69bade0d6415e6aeeddd97ceea0
sharathjapa <sharath.japa@siu.edu>
2016-04-05 02:00:59 -0500

    Service Alerts
    Service Alerts for the registered user are updated!

--- AIRLINE_BOOKING_MGMT_SYSTEM.cs
+++ AIRLINE_BOOKING_MGMT_SYSTEM.cs
@@ -8,16 +8,17 @@ using System.Text;
 using System.Windows.Forms;
 using atm.USER_FORMS;
 
+
 namespace atm
 {
-    public class AIRLINE_BOOKING_MGMT_SYSTEM: Form
+    public partial class air_tic_rs : Form
     {
         string uname = "";
-        public AIRLINE_BOOKING_MGMT_SYSTEM()
+        public air_tic_rs()
         {
             InitializeComponent();
         }
-        public AIRLINE_BOOKING_MGMT_SYSTEM(string s)
+        public air_tic_rs(string s )
         {
             uname = s;
             InitializeComponent();
@@ -25,43 +26,102 @@ namespace atm
 
       
 
-        private void SearchToolStripMenuItem_Click(object sender, EventArgs e)
+        private void sEARCHToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DisposeAllButThis();
             Search sc = new Search(lbshwuser.Text);
             sc.MdiParent = this;
             sc.Show();
         }
-		 private void EditProfileToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void eDITPROFILEToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DisposeAllButThis();
             EditProfile EP = new EditProfile(lbshwuser.Text);
             EP.MdiParent = this;
             EP.Show();
-           
         }
-	private void BookingToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void bOOKINGToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DisposeAllButThis();
-            Search sc = new Search(lbshwuser.Text);
-            sc.MdiParent = this;
-            sc.Show();
+            Booking b = new Booking(lbshwuser.Text);
+            b.MdiParent = this;
+            b.Show();
         }
-		private void BookedTicketsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            DisposeAllButThis();
-            BOOKED_TICKET bt = new BOOKED_TICKET(lbshwuser.Text);
-            bt.MdiParent = this;
-            bt.Show();
-            
-        }
-         private void CancellationToolStripMenuItem_Click(object sender, EventArgs e)
+
+        private void cANCELLATIONToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DisposeAllButThis();
             atm.USER_FORMS.CancelFrm cf = new atm.USER_FORMS.CancelFrm(lbshwuser.Text,"");
             cf.MdiParent = this;
             cf.Show();
            
+        }
+
+        private void air_tic_rs_Load(object sender, EventArgs e)
+        {
+            lbshwuser.Text = uname;
+            gb1.Hide();
+            gb2.Hide();
+            gb3.Hide();
+            gb4.Hide();
+            string s="select R_id,Reservation_date from Reserve_Info where cust_email='"+uname+"'";
+            DataTable dt = dataaccess_layer.GetRecords(s);
+            if (dt.Rows.Count == 1)
+            {
+                gb1.Show();
+                TimeSpan ts = DateTime.Now.Date.Subtract(Convert.ToDateTime(dt.Rows[0][1]).Date);
+                if (ts.Days <= 7)
+                {
+                    lblreservation1.Text = dt.Rows[0][0].ToString();
+                    lbldate1.Text = dt.Rows[0][1].ToString();
+                }
+            }
+            if (dt.Rows.Count > 1)
+            {
+                gb1.Show();
+                gb2.Show();
+                TimeSpan ts = DateTime.Now.Date.Subtract(Convert.ToDateTime(dt.Rows[0][1]).Date);
+                TimeSpan ts1 = DateTime.Now.Date.Subtract(Convert.ToDateTime(dt.Rows[1][1]).Date);
+                if (ts.Days <= 7)
+                {
+                    lblreservation1.Text = dt.Rows[0][0].ToString();
+                    lbldate1.Text = dt.Rows[0][1].ToString();
+                }
+                if (ts1.Days <= 7)
+                {
+                    lblreservation2.Text = dt.Rows[1][0].ToString();
+                    lbldate2.Text = dt.Rows[1][1].ToString();
+                }
+            }
+            string s1 = "SELECT a.src, a.dest, b.DOJ FROM Flight_master AS a INNER JOIN BOOKING_INFO AS b ON a.F_No = b.F_NO WHERE (b.CID =(SELECT cid FROM Customer_Info WHERE (Email = '"+uname+"')))";
+            DataTable dt1 = dataaccess_layer.GetRecords(s1);
+            if (dt1.Rows.Count == 1)
+            {
+                gb3.Show();
+                lbsrc.Text = dt1.Rows[0][0].ToString();
+                lbdest.Text = dt1.Rows[0][1].ToString();
+                TimeSpan ts2 = (Convert.ToDateTime(dt1.Rows[0][2]).Date).Subtract(DateTime.Now.Date);
+                lbdays.Text = ts2.Days.ToString();
+            }
+            if (dt1.Rows.Count > 1)
+            {
+                gb3.Show();
+                lbsrc.Text = dt1.Rows[0][0].ToString();
+                lbdest.Text = dt1.Rows[0][1].ToString();
+                TimeSpan ts2 = (Convert.ToDateTime(dt1.Rows[0][2]).Date).Subtract(DateTime.Now.Date);
+                lbdays.Text = ts2.Days.ToString();
+                gb4.Show();
+                lblsrc1.Text = dt1.Rows[1][0].ToString();
+                lbldest1.Text = dt1.Rows[1][1].ToString();
+                TimeSpan ts3 = (Convert.ToDateTime(dt1.Rows[1][2]).Date).Subtract(DateTime.Now.Date);
+                lbldays1.Text = ts3.Days.ToString();
+            }
+            if ((dt.Rows.Count == 0) && (dt1.Rows.Count == 0))
+            {
+                gbalerts.Hide();
+            }
         }
         public void DisposeAllButThis()
         {
@@ -71,16 +131,48 @@ namespace atm
                 return;
             }
         }
-	private void lnklblogout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        private void bOOKEDTICKETSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DisposeAllButThis();
+            BOOKED_TICKET bt = new BOOKED_TICKET(lbshwuser.Text);
+            bt.MdiParent = this;
+            bt.Show();
+            
+        }
+
+        private void lnklblogout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             this.Close();
             USER_LOGIN ul = new USER_LOGIN();
             ul.Show();
         }
-        private void AIRLINE_BOOKING_MGMT_SYSTEM_Load(object sender, EventArgs e)
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
-            lbshwuser.Text = uname;
+
         }
 
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void rESERVATIONSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DisposeAllButThis();
+            Reservations r = new Reservations(lbshwuser.Text);
+            r.Show();
+        }
+
+        private void btndeactivate_Click(object sender, EventArgs e)
+        {
+            string s = "delete from customer_info where email='" + uname + "'";
+            dataaccess_layer.ProcessQuery(s);
+            USER_LOGIN ul = new USER_LOGIN();
+            ul.Show();
+        }
+
+        
     }
 }
