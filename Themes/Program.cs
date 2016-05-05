fb5b223d9cfda5abcaeb2f6a9bcd0f4a142e50a2
SharathChitmalla <sharathchandra@siu.edu>
2016-05-05 01:33:56 -0500

    Themes

---
+++ Themes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using atm.USER_FORMS;
+
+namespace atm
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new USER_LOGIN());
+           
+        }
+    }
+}
